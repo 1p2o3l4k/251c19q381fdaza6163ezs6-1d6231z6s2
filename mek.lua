cdd33989c880c1b07b74543977a134973ed57e51
1p2o3l4k <dimassherliana@gmail.com>
2024-10-09 12:37:26 +0700

    Update mek.lua

--- mek.lua
+++ mek.lua
@@ -13,3 +13,11 @@ getgenv().Games = {
     [17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 3
     [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
   }
+
+local placeId = game.PlaceId
+if getgenv().Games[placeId] then
+    local scriptUrl = getgenv().Games[placeId]
+    loadstring(game:HttpGet(scriptUrl))()
+else
+    print("Script untuk game ini tidak tersedia.")
+end
